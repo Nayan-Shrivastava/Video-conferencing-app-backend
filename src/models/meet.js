edb2a0995831ea1c3e23b4c96fe94c03af343d34
Purva-Joshi-043 <purvaj043@gmail.com>
2022-04-18 17:57:11 +0530

    added model for meet

---
+++ src/models/meet.js
@@ -0,0 +1,66 @@
+import { model, Schema } from 'mongoose';
+
+const MeetingTypes = {
+  OPEN_TO_ALL: 0,
+  ORGANIZATION: 2,
+  RESTRICTED: 1,
+};
+
+const meetSchema = new Schema(
+  {
+    endTime: {
+      type: Date,
+    },
+    host: {
+      ref: 'User',
+      required: true,
+      trim: true,
+      type: Schema.Types.ObjectId,
+    },
+    joinedMemberList: [
+      {
+        ref: 'User',
+        required: true,
+        type: Schema.Types.ObjectId,
+      },
+    ],
+    logs: [
+      {
+        message: {
+          type: String,
+        },
+        time: {
+          default: new Date(),
+          type: Date,
+        },
+      },
+    ],
+    meetingCode: {
+      required: true,
+      type: String,
+      unique: true,
+    },
+    numberOfParticipants: {
+      type: Number,
+    },
+    orgId: {
+      ref: 'Organization',
+      trim: true,
+      type: Schema.Types.ObjectId,
+    },
+    startTime: {
+      type: Date,
+    },
+    title: {
+      type: String,
+    },
+    type: {
+      default: MeetingTypes.OPEN_TO_ALL,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const Meet = model('Meet', meetSchema);
