c9eb3afc8288b9b70921026764cf284ee3096c07
Ashuto7h <ashusahu198@gmail.com>
2022-04-20 12:48:06 +0530

    added ts-node for dev

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import { createServer } from 'http';
 import { ExpressPeerServer } from 'peer';
+import cors from 'cors';
 import config from './configs';
 import { logger } from './utils/logger';
 import { initializeSocket } from './socketInstance';
@@ -11,14 +12,16 @@ const httpSever = createServer(app);
 initializeSocket(httpSever);
 
 app.use(express.json());
-app.use((req, res, next) => {
-  const { origin } = req.headers;
-  if (origin) res.setHeader('Access-Control-Allow-Origin', origin);
-  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', true);
-  next();
-});
+app.use(
+  cors({
+    allowedHeaders: ['Content-Type'],
+    credentials: true,
+    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+    origin: (o, cb) => {
+      cb(null, true);
+    },
+  }),
+);
 
 mongoose.connect(config.mongoDBUrl, {
   useNewUrlParser: true,
